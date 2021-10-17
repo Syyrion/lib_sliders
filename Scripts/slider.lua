a8f329ea74da0b4f1ace08ccc7ba67d2d497c4fc
Syyrion <cuiricky4@gmail.com>
2021-10-17 11:16:46 -0400

    Changed behavior of X offset of sliders

--- Scripts/slider.lua
+++ Scripts/slider.lua
@@ -11,7 +11,7 @@ Slider.__index = Slider
 -- Y-Offset = 0 [units]
 -- X-Offset = 0 [units]
 -- Value = 0 [units]
-function Slider:new(p, a, y, x, t, func)
+function Slider:new(p, a, y, x, t, fn)
 	local newInst = {}
 	setmetatable(newInst, self)
 
@@ -20,52 +20,53 @@ function Slider:new(p, a, y, x, t, func)
 	newInst:setYOffset(y)
 	newInst:setXOffset(x)
 	newInst:setTimescale(t)
-	newInst:setFunction(func)
+	newInst:setFunction(fn)
+	newInst.xpos = newInst.zero
 	newInst.value = 0
 
 	return newInst
 end
 
 function Slider:setPeriod(p)
-	p = p and p > 0 and p or 1
-	if self.x then self.x = map(self.x, 0, self.period, 0, p) end
+	p = type(p) == 'number' and p > 0 and p or 1
+	if self.xpos then self.xpos = map(self.xpos, self.zero, self.zero + self.period , self.zero, self.zero + p) end
 	self.period = p
 end
 
 function Slider:getPeriod() return self.period end
 
-function Slider:setAmplitude(a) self.amplitude = a or 1 end
+function Slider:setAmplitude(a) self.amplitude = type(a) == 'number' and a or 1 end
 
 function Slider:getAmplitude() return self.amplitude end
 
-function Slider:setYOffset(y) self.yOffset = y or 0 end
+function Slider:setYOffset(y) self.yOffset = type(y) == 'number' and y or 0 end
 
 function Slider:getYOffset() return self.yOffset end
 
-function Slider:setXOffset(x) self.x = x and clamp(x, 0, self.period) or 0 end
+function Slider:setXOffset(x) self.zero = type(x) == 'number' and x or 0 end
 
-function Slider:getXOffset() return self.x end
+function Slider:getXOffset() return self.xpos end
 
-function Slider:setTimescale(t) self.timescale = t or 1 end
+function Slider:setTimescale(t) self.timescale = type(t) == 'number' and t or 1 end
 
 function Slider:getTimescale() return self.timescale end
 
-function Slider:setFunction(func) self.func = type(func) == 'function' and func or function() end end
+function Slider:setFunction(fn) self.fn = type(fn) == 'function' and fn or function() end end
 
-function Slider:getFunction() return self.func end
+function Slider:getFunction() return self.fn end
 
-function Slider:setValue(v) self.value = v or 0 end
+function Slider:setValue(v) self.value = type(v) == 'number' and v or 0 end
 
 function Slider:getValue() return self.value end
 
 function Slider:advance(mFrameTime, ...)
-	self.x = self.x + clamp(mFrameTime / FPS * self.timescale, -self.period, self.period)
-	if self.x >= self.period then
-		self.x = self.x - self.period
-		self.func(...)
-	elseif self.x <= 0 then
-		self.x = self.x + self.period
-		self.func(...)
+	self.xpos = self.xpos + clamp(mFrameTime / FPS * self.timescale, -self.period, self.period)
+	if self.zero + self.xpos >= self.zero + self.period then
+		self.xpos = self.xpos - self.period
+		self.fn(...)
+	elseif self.zero + self.xpos <= self.zero then
+		self.xpos = self.xpos + self.period
+		self.fn(...)
 	end
 end
 
@@ -82,7 +83,7 @@ setmetatable(SliderSquare, Slider)
 -- Default value for dutyCycle
 SliderSquare.dutyCycle = 0.5
 
-function SliderSquare:new(p, a, y, x, t, d, func)
+function SliderSquare:new(p, a, y, x, t, d, fn)
 	local newInst = {}
 	setmetatable(newInst, self)
 
@@ -92,19 +93,19 @@ function SliderSquare:new(p, a, y, x, t, d, func)
 	newInst:setXOffset(x)
 	newInst:setTimescale(t)
 	newInst:setDutyCycle(d)
-	newInst:setFunction(func)
+	newInst:setFunction(fn)
 	newInst.value = 0
 
 	return newInst
 end
 
-function SliderSquare:setDutyCycle(d) self.dutyCycle = d and clamp(d, 0, 1) or 0.5 end
+function SliderSquare:setDutyCycle(d) self.dutyCycle = type(d) == 'number' and clamp(d, 0, 1) or 0.5 end
 
 function SliderSquare:getDutyCycle() return self.dutyCycle end
 
 function SliderSquare:step(mFrameTime, ...)
 	self:advance(mFrameTime, ...)
-	self.value = self.amplitude * square(self.x, self.period, self.dutyCycle) + self.yOffset
+	self.value = self.amplitude * math.square(self.zero + self.xpos, self.period, self.dutyCycle) + self.yOffset
 end
 
 
@@ -119,7 +120,7 @@ setmetatable(SliderTriangle, Slider)
 
 function SliderTriangle:step(mFrameTime, ...)
 	self:advance(mFrameTime, ...)
-	self.value = self.amplitude * triangle(self.x, self.period) + self.yOffset
+	self.value = self.amplitude * math.triangle(self.zero + self.xpos, self.period) + self.yOffset
 end
 
 
@@ -133,7 +134,7 @@ setmetatable(SliderSawtooth, Slider)
 
 function SliderSawtooth:step(mFrameTime, ...)
 	self:advance(mFrameTime, ...)
-	self.value = self.amplitude * sawtooth(self.x, self.period) + self.yOffset
+	self.value = self.amplitude * math.sawtooth(self.zero + self.xpos, self.period) + self.yOffset
 end
 
 
@@ -147,7 +148,21 @@ setmetatable(SliderSine, Slider)
 
 function SliderSine:step(mFrameTime, ...)
 	self:advance(mFrameTime, ...)
-	self.value = self.amplitude * math.sin(math.tau * self.x / self.period) + self.yOffset
+	self.value = self.amplitude * math.sin(math.tau * (self.zero + self.xpos) / self.period) + self.yOffset
+end
+
+
+
+
+-- Cosine wave Slider
+-- Inherits from Slider
+SliderCosine = {}
+SliderCosine.__index = SliderCosine
+setmetatable(SliderCosine, Slider)
+
+function SliderCosine:step(mFrameTime, ...)
+	self:advance(mFrameTime, ...)
+	self.value = self.amplitude * math.cos(math.tau * (self.zero + self.xpos) / self.period) + self.yOffset
 end
 
 
@@ -176,24 +191,24 @@ function SliderManual:new(decKey, incKey, delta, start, min, max, loop)
 	newInst:setIncKey(incKey)
 	newInst:setDelta(delta)
 	newInst:setLimits(min, max, loop)
-	
+
 	return newInst
 end
 
-function SliderManual:setDecKey(decKey) self.decKey = decKey or -1 end
+function SliderManual:setDecKey(decKey) self.decKey = type(decKey) == 'number' and math.floor(decKey) or -1 end
 
 function SliderManual:getDecKey() return self.decKey end
 
-function SliderManual:setIncKey(incKey) self.incKey = incKey or -1 end
+function SliderManual:setIncKey(incKey) self.incKey = type(incKey) == 'number' and math.floor(incKey) or -1 end
 
 function SliderManual:getIncKey() return self.incKey end
 
-function SliderManual:setDelta(delta) self.delta = delta or 1 end
+function SliderManual:setDelta(delta) self.delta = type(delta) == 'number' and delta or 1 end
 
 function SliderManual:getDelta() return self.delta end
 
 function SliderManual:setLimits(min, max, loop)
-	if min and max and min <= max then
+	if type(min) == 'number' and type(max) == 'number' and min <= max then
 		self.min, self.max = min, max
 		if loop then self.range = max - min end
 	end
@@ -203,7 +218,7 @@ function SliderManual:removeLimits() self.min, self.max, self.range = nil, nil, 
 
 function SliderManual:getLimits() return self.min, self.max end
 
-function SliderManual:setValue(v) self.value = v and (self.min and self.max and clamp(v, self.min, self.max) or v) or (self.min and self.max and clamp(0, self.min, self.max) or 0) end
+function SliderManual:setValue(v) self.value = type(v) == 'number' and (self.min and self.max and clamp(v, self.min, self.max) or v) or (self.min and self.max and clamp(0, self.min, self.max) or 0) end
 
 function SliderManual:getValue() return self.value end
 
@@ -235,7 +250,7 @@ SliderPerlin.M, SliderPerlin.A, SliderPerlin.C = 4294967296, 1664525, 1
 -- <s> Seed value. Must be a number between 0 and 1. Seed can only be set once for a slider.
 -- <x> X-Offset. Not very useful since outputs are random.
 -- Passing nothing for a parameter will set the parameter to it's default
-function SliderPerlin:new(p, a, y, t, s, x, func)
+function SliderPerlin:new(p, a, y, t, s, x, fn)
 	local newInst = {}
 	setmetatable(newInst, {__index = self})
 
@@ -244,7 +259,7 @@ function SliderPerlin:new(p, a, y, t, s, x, func)
 	newInst:setYOffset(y)
 	newInst:setTimescale(t)
 	newInst:setXOffset(x)
-	newInst:setFunction(func)
+	newInst:setFunction(fn)
 	newInst.value = 0
 	newInst.Z = math.floor((s or u_rndReal()) * newInst.M)
 	newInst.yBegin = newInst.amplitude * (newInst:randLCG() * 2 - 1) + newInst.yOffset
@@ -254,25 +269,25 @@ function SliderPerlin:new(p, a, y, t, s, x, func)
 end
 
 -- Override default timescale functions as the Perlin slider doesn't support negative timescales
-function SliderPerlin:setTimescale(t) self.timescale = t and math.max(t, 0) or 1 end
+function SliderPerlin:setTimescale(t) self.timescale = type(t) == 'number' and math.max(t, 0) or 1 end
 
 function SliderPerlin:getTimescale() return self.timescale end
 
 -- Advances x value
 -- When wraps around, rolls the next critical point
 function SliderPerlin:advance(mFrameTime, ...)
-	self.x = self.x + clamp(mFrameTime / FPS * self.timescale, -self.period, self.period)
-	if self.x >= self.period then
+	self.xpos = self.xpos + clamp(mFrameTime / FPS * self.timescale, -self.period, self.period)
+	if self.xpos >= self.period then
 		self:next()
-		self.func(...)
-		self.x = self.x - self.period
+		self.fn(...)
+		self.xpos = self.xpos - self.period
 	end
 end
 
 -- Updates slider to the next value
 function SliderPerlin:step(mFrameTime, ...)
 	self:advance(mFrameTime, ...)
-	self.value = lerp(self.yBegin, self.yEnd, easeInOutSine(self.x / self.period))
+	self.value = lerp(self.yBegin, self.yEnd, easeInOutSine(self.xpos / self.period))
 end
 
 -- Linear Congruential Generator. Separate PRNG that can be seeded. Unique to each individual slider
@@ -335,7 +350,7 @@ end
 -- Sets all perlin sliders to the same timescale
 function SliderNoise:setTimescale(t)
 	if self.perlinSet then
-		for k, v in pairs(self.perlinSet) do
+		for _, v in pairs(self.perlinSet) do
 			v:setTimescale(t)
 		end
 	end
@@ -347,7 +362,7 @@ function SliderNoise:getTimescale() if self.perlinSet then return self.perlinSet
 function SliderNoise:step(mFrameTime)
 	if self.perlinSet then
 		sum = 0
-		for k, v in pairs(self.perlinSet) do
+		for _, v in pairs(self.perlinSet) do
 			v:step(mFrameTime)
 			sum = sum + v:getValue()
 		end
@@ -369,7 +384,7 @@ SliderTarget.__index = SliderTarget
 -- <ease> Easing function. Must be a function that accepts a value from [0,1] and returns a value from [0,1]
 -- <start> Starting value. This is only set once per class
 -- <t> Timescale.
-function SliderTarget:new(dur, ease, start, t, func)
+function SliderTarget:new(dur, ease, start, t, fn)
 	local newInst = {}
 	setmetatable(newInst, self)
 
@@ -381,16 +396,16 @@ function SliderTarget:new(dur, ease, start, t, func)
 	newInst:setEaseFunction(ease)
 	newInst:setValue(start)
 	newInst:setTimescale(t)
-	newInst:setFunction(func)
+	newInst:setFunction(fn)
 
 	return newInst
 end
 
-function SliderTarget:setDuration(dur) self.duration = dur and dur > 0 and dur or 1 end
+function SliderTarget:setDuration(dur) self.duration = type(dur) == 'number' and dur > 0 and dur or 1 end
 
 function SliderTarget:getDuration() return self.duration end
 
-function SliderTarget:setTimescale(t) self.timescale = t and math.max(t, 0) or 1 end
+function SliderTarget:setTimescale(t) self.timescale = type(t) == 'number' and math.max(t, 0) or 1 end
 
 function SliderTarget:getTimescale() return self.timescale end
 
@@ -398,14 +413,14 @@ function SliderTarget:setEaseFunction(ease) self.ease = type(ease) == 'function'
 
 function SliderTarget:getEaseFunction() return self.ease end
 
-function SliderTarget:setFunction(func) self.func = type(func) == 'function' and func or function() end end
+function SliderTarget:setFunction(fn) self.fn = type(fn) == 'function' and fn or function() end end
 
-function SliderTarget:getFunction() return self.func end
+function SliderTarget:getFunction() return self.fn end
 
 -- Sets the value of the slider and freezes the slider
 function SliderTarget:setValue(v)
-	self.x = 1
-	v = v or 0
+	self.xpos = 1
+	v = type(v) == 'number' and v or 0
 	self.value = v
 	self.target = v
 end
@@ -419,18 +434,18 @@ function SliderTarget:newTarget(target)
 	if target ~= self.target then
 		self.target = target
 		self.start = self.value
-		self.x = target == self.value and 1 or 0
+		self.xpos = target == self.value and 1 or 0
 	end
 end
 
 -- Advances the slider until target is reached
 function SliderTarget:step(mFrameTime, ...)
-	if self.x < 1 then
-		self.x = self.x + mFrameTime / FPS / self.duration * self.timescale
-		if self.x > 1 then
-			self.x = 1
-			self.func(...)
+	if self.xpos < 1 then
+		self.xpos = self.xpos + mFrameTime / FPS / self.duration * self.timescale
+		if self.xpos > 1 then
+			self.xpos = 1
+			self.fn(...)
 		end
-		self.value = lerp(self.start, self.target, self.ease(self.x))
+		self.value = lerp(self.start, self.target, self.ease(self.xpos))
 	end
 end
--- Scripts/timer.lua
+++ Scripts/timer.lua
@@ -1,35 +1,40 @@
+local Timer = {}
+Timer.__index = Timer
+
+function Timer:setFunction(fn) self.fn = type(fn) == 'function' and fn or function() end end
+
+function Timer:getFunction() return self.fn end
+
+function Timer:setTimescale(t) self.timescale = type(t) == 'number' and math.max(t, 0) or 1 end
+
+function Timer:getTimescale() return self.timescale end
+
+
+
+
 -- Periodic Timer
 -- Runs a function at set intervals
-TimerPeriodic = {}
+TimerPeriodic = setmetatable({}, Timer)
 TimerPeriodic.__index = TimerPeriodic
 
 -- <dur> Seconds between function calls
--- <func> Timer function
+-- <fn> Timer function
 -- <noInstantRun> Set to true to disable the first timer call
 -- <t> Timescale
-function TimerPeriodic:new(dur, func, noInstantRun, t)
-	local newInst = {}
-	setmetatable(newInst, self)
+function TimerPeriodic:new(dur, fn, noInstantRun, t)
+	local newInst = setmetatable({}, self)
 
 	newInst.x = noInstantRun and 0 or 1
 	newInst:setDuration(dur)
-	newInst:setFunction(func)
+	newInst:setFunction(fn)
 	newInst:setTimescale(t)
 
 	return newInst
 end
 
-function TimerPeriodic:setDuration(dur) self.duration = dur and dur > 0 and dur or 1 end
+function TimerPeriodic:setDuration(dur) self.duration = type(dur) == 'number' and dur > 0 and dur or 1 end
 
 function TimerPeriodic:getDuration() return self.duration end
-
-function TimerPeriodic:setFunction(func) self.func = type(func) == 'function' and func or function() end end
-
-function TimerPeriodic:getFunction() return self.func end
-
-function TimerPeriodic:setTimescale(t) self.timescale = t and math.max(t, 0) or 1 end
-
-function TimerPeriodic:getTimescale() return self.timescale end
 
 -- Resets the timer cycle. If noInstantRun is false or nil, the timer function will be immedietly run (assuming the step function is also being consistently called)
 function TimerPeriodic:reset(noInstantRun) self.x = noInstantRun and 0 or 1 end
@@ -40,7 +45,7 @@ function TimerPeriodic:step(mFrameTime, ...)
 	self.x = self.x + clamp(mFrameTime / FPS / self.duration * self.timescale, 0, 1)
 	if self.x >= 1 then
 		self.x = self.x - 1
-		return self.func(...)
+		return self.fn(...)
 	end
 end
 
@@ -48,20 +53,19 @@ end
 
 -- Delay Timer
 -- Runs a function after a delay
-TimerDelay = {}
+TimerDelay = setmetatable({}, Timer)
 TimerDelay.__index = TimerDelay
 
 -- <delay> Delay value. If set, class will automatically trigger a new delay event. If nil or <= 0, class will wait for a newDelay call
--- <func> Function to run
+-- <fn> Function to run
 -- <t> Timescale
 -- <noInstantRun> Set to true to disable running the delay immedietly when created. Useful for disabling this behavior even when the delay value is set, This value is ignored if delay is nil
 
-function TimerDelay:new(delay, func, t, noInstantRun)
-	local newInst = {}
-	setmetatable(newInst, self)
+function TimerDelay:new(delay, fn, t, noInstantRun)
+	local newInst = setmetatable({}, self)
 
-	newInst:setFunction(func)
-	if delay then
+	newInst:setFunction(fn)
+	if type(delay) == 'number' then
 		newInst.delay = math.max(delay, 0)
 		newInst.x = noInstantRun and 1 or 0
 	else
@@ -69,24 +73,16 @@ function TimerDelay:new(delay, func, t, noInstantRun)
 		newInst.x = 1
 	end
 	newInst:setTimescale(t)
-	
+
 	return newInst
 end
 
 -- <delay> If delay > 0, will wait that amount of time before running the function. If delay <= 0, will run the function immedietly. If nil, will repeat the last delay amount
 
 function TimerDelay:newDelay(delay)
-	if delay then self.delay = math.max(delay, 0) end
+	if type(delay) == 'number' then self.delay = math.max(delay, 0) end
 	self.x = 0
 end
-
-function TimerDelay:setFunction(func) self.func = type(func) == 'function' and func or function() end end
-
-function TimerDelay:getFunction() return self.func end
-
-function TimerDelay:setTimescale(t) self.timescale = t and math.max(t, 0) or 1 end
-
-function TimerDelay:getTimescale() return self.timescale end
 
 -- Extra parameters can be passed to the step function
 -- Step function will return anything that the assigned function returns
@@ -94,10 +90,41 @@ function TimerDelay:step(mFrameTime, ...)
 	if self.x < 1 then
 		if self.delay == 0 then
 			self.x = 1
-			return self.func(...)
+			return self.fn(...)
 		end
 		self.x = self.x + mFrameTime / FPS / self.delay * self.timescale
 		if self.x > 1 then self.x = 1 end
-		if self.x == 1 then return self.func(...) end
+		if self.x == 1 then return self.fn(...) end
+	end
+end
+
+
+
+
+
+TimerTick = setmetatable({}, Timer)
+TimerTick.__index = TimerTick
+
+function TimerTick:new(tps, fn)
+	local newInst = setmetatable({t = 0}, self)
+
+	newInst:setTps(tps)
+	newInst:setFunction(fn)
+
+	return newInst
+end
+
+function TimerTick:setTps(tps)
+	self.ft = type(tps) == 'number' and FPS / tps or 1
+end
+function TimerTick:getTps()
+	return FPS / self.ft
+end
+
+function TimerTick:step(mFrameTime, ...)
+	self.t = self.t + mFrameTime
+	while self.t - self.ft > 0 do
+		self.fn(self.ft, ...)
+		self.t = self.t - self.ft
 	end
 end
