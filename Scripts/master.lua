7e916a1959b81c6758fed6dc93d38276fa211290
Syyrion <cuiricky4@gmail.com>
2022-01-16 11:59:20 -0500

    Add licence

--- Scripts/master.lua
+++ Scripts/master.lua
@@ -1,3 +1,26 @@
+--[[
+	Precision variable control for Open Hexagon.
+	https://github.com/vittorioromeo/SSVOpenHexagon
+
+	Copyright (C) 2021 Ricky Cui
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+	Email: cuiricky4@gmail.com
+	GitHub: https://github.com/Syyrion
+]]
+
 u_execDependencyScript("library_extbase", "extbase", "syyrion", "utils.lua")
 
 local __FUNCTION = {
@@ -465,9 +488,9 @@ function Keyframe:sequence(...)
 				['e'] = 'event',
 				['c'] = 'copy',
 				['t'] = 'terminate'
-			})[t[i].mode or 'e'] or error(('Argument #%d has an invalid mode.'):format(i), 2)](self, unpack(t[i]))
+			})[t[i].mode or 'e'] or errorf(2, 'Sequence', 'Argument #%d has an invalid mode.', i)](self, unpack(t[i]))
 		else
-			error(('Argument #%d is not a table.'):format(i), 2)
+			errorf(2, 'Sequence', 'Argument #%d is not a table.', i)
 		end
 	end
 end
